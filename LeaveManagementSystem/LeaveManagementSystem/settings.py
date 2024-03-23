7a859631537d816bfdd90178337f6bf07d20c9c2
visaal1582892 <visaal1582892@gmail.com>
2024-03-23 08:06:02 +0530

    c

--- LeaveManagementSystem/LeaveManagementSystem/settings.py
+++ LeaveManagementSystem/LeaveManagementSystem/settings.py
@@ -88,15 +88,8 @@ WSGI_APPLICATION = 'LeaveManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
